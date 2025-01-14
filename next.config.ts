335bfec811d91baaaa643c5209c481dcf51a4dec
Down <russelnitullano08@gmail.com>
2025-01-14 11:33:08 +0800

    fixing deployment issue

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Optional: Generate static HTML for pages that are pre-rendered
-  trailingSlash: true,  // Optional, generates a trailing slash for static URLs
-
-  // Optionally, customize the build output folder (if you want to specify a different folder than `out`)
-  distDir: 'out',  // Firebase Hosting expects static files in the 'out' folder
-
-  // Other Next.js configurations can go here
 };
 
 export default nextConfig;
